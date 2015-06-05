2b732336aecc250e972c716e86a805f6cc57c1e2
Jeeeyul <jeeeyul@gmail.com>
2015-06-06 00:35:33 +0900

    simplify build

---
+++ beta-update/build.sh
@@ -0,0 +1,18 @@
+rm artifacts.* content.*
+cp ../beta-template/* .
+eclipse \
+	-nosplash \
+	-application org.eclipse.equinox.p2.publisher.FeaturesAndBundlesPublisher \
+	-metadataRepository file:$PWD \
+	-artifactRepository file:$PWD \
+	-source $PWD \
+	-publishArtifact \
+	-compress
+eclipse\
+	-nosplash \
+	-console -consolelog \
+	-application org.eclipse.equinox.p2.publisher.CategoryPublisher \
+	-metadataRepository file:$PWD \
+	-categoryDefinition file:$PWD/category.xml \
+	-categoryQualifier \
+	-compress
